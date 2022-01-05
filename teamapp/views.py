e481cfc0b364db769ad649b44fe37796c3a8f9fe
s1f102101244 <85746244+s1f102101244@users.noreply.github.com>
2022-01-05 17:57:57 +0900

    unnsei

--- teamapp/views.py
+++ teamapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+
+fortune = ['大吉', '吉', '中吉', '小吉', '末吉', '凶', '大凶']
 
 # Create your views here.
 
@@ -7,4 +10,8 @@ def index(request):
 	return HttpResponse('Hello Django')
 
 def index(request):
-	return render(request, 'teamapp/index.html')
+	data = {
+		'fortune' : random.choices(fortune)
+    }
+	return render(request, 'teamapp/index.html', data)
+
