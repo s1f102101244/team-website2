5243ea99b2ac9407e76c30e071b55107574a0a2e
s1f102101244 <85746244+s1f102101244@users.noreply.github.com>
2022-01-05 16:15:23 +0900

    teamapp
    teamapp

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from teamapp import views as teamapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('teamapp/', teamapp_views.index)
 ]
