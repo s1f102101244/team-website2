56683d6c5b8169b7d6bb6598bc53be8852b825e3
s1f102101244 <85746244+s1f102101244@users.noreply.github.com>
2022-01-05 16:24:10 +0900

    change

--- config/urls.py
+++ config/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from teamapp import views as teamapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('teamapp/', teamapp_views.index)
+    path('teamapp/', include('teamapp.urls')),
+
 ]
